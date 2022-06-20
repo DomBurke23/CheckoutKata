292fb6aa685c3f63819b758b0e1265e5b157f74d
Dominique Burke <dominiqueburke@hotmail.co.uk>
2022-06-20 20:00:54 +0100

    null bag expect exception test.

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,32 @@
+﻿using CheckoutKata.Models;
+
+namespace CheckoutKata
+{
+    public class Checkout
+    {
+        public Checkout()
+        {
+        }
+
+        static public void Main(String[] args)
+        {
+            Console.WriteLine("Main Method");
+        }
+
+        public double CalculateTotal(IList<Item> itemsInBag)
+        {
+            try
+            {
+                if (!itemsInBag.Any())
+                {
+                    return 0;
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CheckoutKataContractTests/CheckoutKataTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using CheckoutKata;
+using CheckoutKata.Models;
+using Xunit;
+
+namespace CheckoutKataContractTests
+{
+    public class CheckoutKataTests
+    {
+        [Fact]
+        public void NoItems_ReturnZero()
+        {
+            var checkout = new Checkout();
+            IList<Item> bag = null;
+            var ex = Assert.Throws<ArgumentNullException>(() => checkout.CalculateTotal(bag));
+            Assert.Contains("Value cannot be null. ", ex.Message);
+        }
+    }
+}
