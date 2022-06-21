44a868c2df475e9ac2e15e618652a3c28d77f7c4
Dominique Burke <dominiqueburke@hotmail.co.uk>
2022-06-21 10:41:28 +0100

    if there are 2 Bs in bag apply discount.

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -24,12 +24,28 @@ namespace CheckoutKata
 
                 var groupedByItem = itemsInBag.GroupBy(x => x.Name);
 
+                var totalCost = itemsInBag.Sum(x => x.Price);
+
                 foreach (var item in groupedByItem)
                 {
                     var groupedSum = item.Sum(i => i.Price);
-                }
+                    var itemCount = item.Count();
 
-                return itemsInBag.Sum(x => x.Price);
+                    // calculate discount 
+                    if (item.Key == "A")
+                    {
+                        // Item A 20% off if you buy 3 products
+                    }
+                    else if(item.Key == "B" && itemCount >= 2)
+                    {
+                        // Item B 25% off if you buy 2 products
+                        if (itemCount % 2 == 0)
+                        {
+                            totalCost = groupedSum - (groupedSum * 0.25);
+                        }
+                    }
+                }
+                return totalCost;
             }
             catch (Exception)
             {
